c1f188360795d02f199d522cb27143f3f2e9d889
Anton Halauko <anton.halauko.main@gmail.com>
2024-04-05 15:59:13 +0300

    feat: implement creating presigned url

--- import-service/src/functions/import-products-file/handler.ts
+++ import-service/src/functions/import-products-file/handler.ts
@@ -1,6 +1,16 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
-import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
+
+const REGION = 'eu-central-1';
+const BUCKET_NAME = 'import-service-bucket-anton';
+
+const createPresignedUrlWithClient = ({ region, bucket, key }) => {
+  const client = new S3Client({ region });
+  const command = new PutObjectCommand({ Bucket: bucket, Key: key });
+  return getSignedUrl(client, command, { expiresIn: 3600 });
+};
 
 const handler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
   const name = event.queryStringParameters?.name;
@@ -14,7 +24,16 @@ const handler: ValidatedEventAPIGatewayProxyEvent<unknown> = async (event) => {
     };
   }
 
-  return formatJSONResponse(event.queryStringParameters);
+  const signedUrl = await createPresignedUrlWithClient({
+    region: REGION,
+    bucket: BUCKET_NAME,
+    key: name,
+  });
+
+  return {
+    statusCode: 200,
+    body: signedUrl,
+  };
 };
 
 export const importProductsFile = middyfy(handler);
